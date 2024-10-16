588b99c97ab934b6d87b4a635f246ff4ab813daf
BratProgrammer <travckin.4@yandex.ru>
2024-10-16 15:23:07 +0300

    удалены ненужные imports

--- src/main/java/com/example/InventoryManagement/Configuration/KafkaConfiguration.java
+++ src/main/java/com/example/InventoryManagement/Configuration/KafkaConfiguration.java
@@ -3,17 +3,11 @@ package com.example.InventoryManagement.Configuration;
 import com.example.InventoryManagement.DTO.Kafka.ProductActionDto;
 import com.example.InventoryManagement.DTO.Kafka.ProductsActionDto;
 import com.fasterxml.jackson.databind.JsonSerializer;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 
